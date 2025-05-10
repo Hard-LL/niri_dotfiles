5399b81e8a694333a300d0d0a0f3ca6b2569afa7
zimu <3241145375@qq.com>
2025-05-10 17:50:08 +0800

    wallpaper.sh

--- niri/.config/niri/wallpaper.sh
+++ niri/.config/niri/wallpaper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-swww img ~/.config/niri/Wallpaper/$(ls ~/.config/niri/Wallpaper | shuf -n 1) --transition-type center
+swww img ~/.dotfiles/niri/.config/niri/Wallpaper/$(ls ~/.dotfiles/niri/.config/niri/Wallpaper | shuf -n 1) --transition-type center
