acc575ca627cb238584f77ce12f88b1a23ba3b61
zimu <3241145375@qq.com>
2025-05-10 09:15:14 +0800

    nvim first

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,3 @@
+require("core.module")
+require("core.keymap")
+require("core.lazy")
---
+++ nvim/.config/nvim/lua/core/keymap.lua
@@ -0,0 +1,8 @@
+-- 保存本地变量
+local map = vim.keymap.set
+local opt = {noremap = true, silent = true }
+ 
+vim.g.mapleader = " "
+ 
+-- 之后就可以这样映射按键了
+-- map('模式','按键','映射为XX',opt)
